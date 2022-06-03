07d071073ffce3eae7cbb3bcc773f2dd487fc30b
rabsef bicrym <rabsef-bicrym@MacBook-Pro-Baby.local>
2022-06-02 22:53:53 -0700

    push change to books.ts, include axios

--- interface/src/store/books.ts
+++ interface/src/store/books.ts
@@ -31,7 +31,26 @@ export default {
       const baseURL = "https://api.zapper.fi/v2/transactions?address=0xee884fE6D8955A12a91Be2ebe897E2FAEA76e594&addresses%5B%5D=0xee884fE6D8955A12a91Be2ebe897E2FAEA76e594"
 
       return new Promise((resolve, reject) => {
-
+        axios({
+          method: 'get',
+          url: baseURL,
+          auth: {
+            username: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+            password: '',
+          },
+          //timeout: 60000,
+          transformResponse: [function (data) {
+            //  it's like cook, baby!
+            console.log(data);
+            return data;
+          }]
+        })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(e => {
+            console.log(e);
+          })
       })
     }
   }
