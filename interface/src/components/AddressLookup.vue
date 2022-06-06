88dedcc3315d8a740e705c1d504cb4fad6a06267
vinney cavallo <vcavallo@gmail.com>
2022-06-06 15:14:37 -0400

    Add tsconfig path setting and use PropType for... prop type

--- interface/src/components/AddressLookup.vue
+++ interface/src/components/AddressLookup.vue
@@ -1,9 +1,11 @@
 <template>
-  {{ nonReservedWord }}
+  <pre>ADDDREESSSSSSSSS {{ nonReservedWord }}</pre>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+import { Address } from "@/types";
 
 export default defineComponent ({
     mounted() {
@@ -23,8 +25,8 @@ export default defineComponent ({
 
     props: {
         nonReservedWord: {
-            type: String,
-            default: null,
+            type: String as PropType<Address>,
+            default: "0x0 (or THERE IS NO ADDRESS HERE)",
         },
     }
 })
